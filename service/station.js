cfcc14b26837127aa48a2d794aa2e71f7968ccce
Danny Tizon <dtizon@gmail.com>
2016-05-15 20:49:30 -0700

    updated stations list

--- service/station.js
+++ service/station.js
@@ -1,7 +1,15 @@
 var HashMap = require('hashmap');
 
 var map = new HashMap();
+
+//station abbr as defined here: http://api.bart.gov/docs/overview/abbrev.aspx
 map
+  .set('12th st. oakland city center','12th')
+  .set('16th st. mision (sf)','16th')
+  .set('19th','19th st. oakland')
+  .set('24th','24th st. mission (sf)')
+  .set('ashb','ashby (berkeley)')
+  .set('balb','balboa park (sf)')
   .set('embarcadero','embr')
   .set('embr','embarcadero')
   .set('fremont','frmt')
